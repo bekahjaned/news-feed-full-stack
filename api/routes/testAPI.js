b8766fa6b0bf5badf69d570343ed901696edd87a
Rebekah <rebekahjanedejong@gmail.com>
2020-12-22 22:33:47 -0500

    First commit

---
+++ api/routes/testAPI.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+router.get("/", async (req, res, next) => {
+    const api_key = process.env.API_KEY;
+    const news_url = `https://api.nytimes.com/svc/news/v3/content/all/all.json?page=50&api-key=${api_key}`
+    const news_response = await fetch(news_url);
+    const news_data = await news_response.json();
+    console.log(news_data)
+
+    res.send([news_data])
+});
+
+module.exports = router;
